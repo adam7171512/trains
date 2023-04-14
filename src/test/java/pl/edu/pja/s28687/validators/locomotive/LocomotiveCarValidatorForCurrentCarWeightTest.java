51081993e4b00fa8f0d5e1d28ee67a985140d08d
adam7 <adam7171512@gmail.com>
2023-04-14 15:01:53 +0200

    change method names and use mock loadValidator

--- src/test/java/pl/edu/pja/s28687/validators/locomotive/LocomotiveCarValidatorForCurrentCarWeightTest.java
+++ src/test/java/pl/edu/pja/s28687/validators/locomotive/LocomotiveCarValidatorForCurrentCarWeightTest.java
@@ -2,37 +2,54 @@ package pl.edu.pja.s28687.validators.locomotive;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pl.edu.pja.s28687.ILocomotive;
 import pl.edu.pja.s28687.Locomotive;
-import pl.edu.pja.s28687.cars.*;
+import pl.edu.pja.s28687.cars.CarType;
+import pl.edu.pja.s28687.cars.ILoadCarrier;
+import pl.edu.pja.s28687.cars.RailroadCar;
 import pl.edu.pja.s28687.factories.CarBuilder;
 import pl.edu.pja.s28687.factories.LocomotiveBuilder;
 import pl.edu.pja.s28687.load.IDeliverable;
-import pl.edu.pja.s28687.load.IToxic;
 import pl.edu.pja.s28687.load.LoadType;
 import pl.edu.pja.s28687.load.ToxicLoad;
 import pl.edu.pja.s28687.logistics.LocoBase;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LocomotiveCarValidatorForCurrentCarWeightTest {
-    private LocoBase locoBase;
-    private ILocomotiveCarValidator validator;
+    private static final LocoBase locoBase = new LocoBase();
+    private static final ILocomotiveCarValidator validator = new LocomotiveCarValidatorForCurrentCarWeight();
+    private static final ILocomotiveLoadValidator loadValidator = new ILocomotiveLoadValidator() {
+        @Override
+        public boolean validate(IDeliverable load, ILocomotive receiver) {
+            return false;
+        }
+
+        @Override
+        public <T extends IDeliverable> List<ILoadCarrier<T>> getCarsThatCouldLoad(IDeliverable load, ILocomotive receiver) {
+            return null;
+        }
+
+        @Override
+        public boolean validateWeight(IDeliverable load, ILocomotive receiver) {
+            return false;
+        }
+    };
     private LocomotiveBuilder locomotiveBuilder;
-    private final ILocomotiveLoadValidator loadValidator = new LocomotiveLoadValidator();
     private CarBuilder carBuilder;
 
     @BeforeEach
     void setUp() {
-        locoBase = new LocoBase();
-        validator = new LocomotiveCarValidatorForCurrentCarWeight();
         locomotiveBuilder = new LocomotiveBuilder(locoBase);
         carBuilder = new CarBuilder(locoBase);
     }
 
-    private Locomotive prepareLoc(int payloadLimit, int carLimit, int poweredCarLimit){
+    private Locomotive prepareLoc(int payloadLimit, int carLimit, int poweredCarLimit) {
         return locomotiveBuilder
                 .setRegularCars(carLimit)
                 .setPoweredCars(poweredCarLimit)
@@ -43,40 +60,40 @@ class LocomotiveCarValidatorForCurrentCarWeightTest {
                 .build();
     }
 
-    private RailroadCar prepareCar(int netWeight, int maxWeight){
+    private RailroadCar prepareCar(int netWeight, int maxWeight) {
         return carBuilder.setFlag(CarType.NON_STANDARD)
                 .setPoweredForNonStandardCar(true).setLoadTypes(Set.of(LoadType.TOXIC)).setShipperForNonStandardCar("test")
                 .setSecurityInfoForNonStandardCar("test").setNumberOfSeatsForNonStandardCar(10)
                 .setMaxWeightForNonStandardCar(maxWeight).setNetWeightForNonStandardCar(netWeight).build();
     }
 
-    private IDeliverable prepareLoad(int weight){
+    private IDeliverable prepareLoad(int weight) {
         return new ToxicLoad(weight);
     }
 
     @Test
-    void Should_ValidatePayload_When_CarWeightIsLessThanLocomotivePayloadLimit() {
+    void validatePayloadLimit_ForCarWeightLowerThanPayloadLimit_ShouldReturnTrue() {
         Locomotive locomotive = prepareLoc(1000, 2, 2);
         RailroadCar car = prepareCar(100, 1200);
         assertTrue(validator.validatePayloadLimit(car, locomotive));
     }
 
     @Test
-    void Should_ValidatePayload_When_CarWeightIsEqualToLocomotivePayloadLimit() {
+    void validatePayloadLimit_ForCarWeightEqualToPayloadLimit_ShouldReturnTrue() {
         Locomotive locomotive = prepareLoc(1000, 2, 2);
         RailroadCar car = prepareCar(1000, 1200);
         assertTrue(validator.validatePayloadLimit(car, locomotive));
     }
 
     @Test
-    void Should_Not_ValidatePayload_WhenCarWeightIsGreaterThanLocomotivePayloadLimit() {
+    void validatePayloadLimit_ForCarWeightGreaterThanPayloadLimit_ShouldReturnFalse() {
         Locomotive locomotive = prepareLoc(1000, 8, 2);
         RailroadCar car = prepareCar(1100, 1200);
         assertFalse(validator.validatePayloadLimit(car, locomotive));
     }
 
     @Test
-    void Should_ValidatePayload_When_CarWeightIsLessThanLocomotiveAvailablePayload() {
+    void validatePayloadLimit_ForCarWeightLowerThanAvailablePayloadLimitWhenOtherCarIsAttached_ShouldReturnTrue() {
         Locomotive locomotive = prepareLoc(1000, 2, 2);
         RailroadCar car = prepareCar(100, 1200);
         locomotive.attach(car);
@@ -85,7 +102,7 @@ class LocomotiveCarValidatorForCurrentCarWeightTest {
     }
 
     @Test
-    void Should_ValidatePayload_When_CarWeightIsEqualToLocomotiveAvailablePayload() {
+    void validatePayloadLimit_ForCarWeightEqualToAvailablePayloadLimitWhenOtherCarIsAttached_ShouldReturnTrue() {
         Locomotive locomotive = prepareLoc(1000, 2, 2);
         RailroadCar car = prepareCar(500, 1200);
         locomotive.attach(car);
@@ -94,7 +111,7 @@ class LocomotiveCarValidatorForCurrentCarWeightTest {
     }
 
     @Test
-    void Should_Not_ValidatePayload_When_CarWeightIsGreaterThanLocomotiveAvailablePayload() {
+    void validatePayloadLimit_ForCarWeightGreaterThanAvailablePayloadLimitWhenOtherCarIsAttached_ShouldReturnFalse() {
         Locomotive locomotive = prepareLoc(1000, 2, 2);
         RailroadCar car = prepareCar(500, 1200);
         locomotive.attach(car);
@@ -103,7 +120,7 @@ class LocomotiveCarValidatorForCurrentCarWeightTest {
     }
 
     @Test
-    void Should_ValidatePayload_When_LoadedCarWeightIsLessThanLocomotiveAvailablePayload() {
+    void validatePayloadLimit_ForCarWeightLowerThanPayloadLimitWhenCarHasCargo_ShouldReturnTrue() {
         Locomotive locomotive = prepareLoc(1000, 2, 2);
         RailroadCar car = prepareCar(500, 1200);
         locomotive.attach(car);
@@ -115,7 +132,7 @@ class LocomotiveCarValidatorForCurrentCarWeightTest {
     }
 
     @Test
-    void Should_ValidatePayload_When_LoadedCarWeightIsEqualToLocomotiveAvailablePayload() {
+    void validatePayloadLimit_ForCarWeightEqualToPayloadLimitWhenCarHasCargo_ShouldReturnTrue() {
         Locomotive locomotive = prepareLoc(1000, 2, 2);
         RailroadCar car = prepareCar(500, 1200);
         locomotive.attach(car);
@@ -127,7 +144,7 @@ class LocomotiveCarValidatorForCurrentCarWeightTest {
     }
 
     @Test
-    void Should_Not_ValidatePayload_When_LoadedCarWeightIsGreaterThanLocomotiveAvailablePayload() {
+    void validatePayloadLimit_ForCarWeightGreaterThanPayloadLimitWhenCarHasCargo_ShouldReturnFalse() {
         Locomotive locomotive = prepareLoc(1000, 2, 2);
         RailroadCar car = prepareCar(500, 1200);
         locomotive.attach(car);
