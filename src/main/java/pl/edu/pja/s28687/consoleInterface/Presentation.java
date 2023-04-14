c372c7b94eccfd2f156f6e5f05dbef7129e10348
adam7 <adam7171512@gmail.com>
2023-04-14 10:57:25 +0200

    add presentation to console menu

---
+++ src/main/java/pl/edu/pja/s28687/consoleInterface/Presentation.java
@@ -0,0 +1,49 @@
+package pl.edu.pja.s28687.consoleInterface;
+
+import pl.edu.pja.s28687.LocomotivePurpose;
+import pl.edu.pja.s28687.factories.*;
+import pl.edu.pja.s28687.gui.Canvas;
+import pl.edu.pja.s28687.load.LoadType;
+import pl.edu.pja.s28687.logistics.LocoBase;
+import pl.edu.pja.s28687.logistics.NaiveRouteFinder;
+
+public class Presentation extends AbstractLeafMenu{
+    @Override
+    public void menuSpecificAction() {
+        TrainStationFactory trainStationFactory = resourceContainer.getTrainStationFactory();
+        TrainSetFactory trainSetFactory = resourceContainer.getTrainSetFactory();
+        CarsFactory carsFactory = resourceContainer.getCarsFactory();
+        LoadFactory loadFactory = resourceContainer.getLoadFactory();
+        RailroadsFactory railroadsFactory = resourceContainer.getRailroadsFactory();
+        LocoBase locoBase = resourceContainer.getLocoBase();
+        DispatchingCenter dispatchingCenter = resourceContainer.getDispatchingCenter();
+
+        trainStationFactory.createTrainStationsPolishCoords();
+        railroadsFactory.createOrderedConnectionsBetweenStations(3);
+
+        trainSetFactory.createTrainSetsOfType(7, LocomotivePurpose.PASSENGER, new NaiveRouteFinder(locoBase));
+        trainSetFactory.createTrainSetsOfType(7, LocomotivePurpose.BASIC_FREIGHT, new NaiveRouteFinder(locoBase));
+        trainSetFactory.createRandomTrainSets(16);
+        carsFactory.createRandomCars(400);
+        loadFactory.createRandomLoads(2000);
+        loadFactory.createRandomLoadsOfType(100, LoadType.PASSENGERS);
+
+        CarAssignmentCenter.assignCars(locoBase);
+        LoadAssignmentCenter.assignLoadsToTrainSets(locoBase);
+
+        dispatchingCenter.dispatchAllTrainSets();
+
+        Canvas canvas= new Canvas(locoBase);
+        canvas.start();
+    }
+
+    @Override
+    public String getTitle() {
+        return "Just prepare and launch the whole thing..";
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
