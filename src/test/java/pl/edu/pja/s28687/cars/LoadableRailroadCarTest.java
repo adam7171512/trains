bedf56c8bc86634f202d340c6a4206719ff5eb7c
adam7 <adam7171512@gmail.com>
2023-04-15 21:17:32 +0200

    fix tests

--- src/test/java/pl/edu/pja/s28687/cars/LoadableRailroadCarTest.java
+++ src/test/java/pl/edu/pja/s28687/cars/LoadableRailroadCarTest.java
@@ -1,123 +1,121 @@
 package pl.edu.pja.s28687.cars;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import pl.edu.pja.s28687.factories.CarBuilder;
+import pl.edu.pja.s28687.ValidationException;
 import pl.edu.pja.s28687.factories.CarsFactory;
-import pl.edu.pja.s28687.factories.LoadBuilder;
 import pl.edu.pja.s28687.factories.LoadFactory;
+import pl.edu.pja.s28687.load.BasicFreightLoad;
 import pl.edu.pja.s28687.load.IDeliverable;
+import pl.edu.pja.s28687.load.LiquidToxicLoad;
 import pl.edu.pja.s28687.load.LoadType;
-import pl.edu.pja.s28687.load.LiquidLoad;
 import pl.edu.pja.s28687.logistics.LocoBase;
+import pl.edu.pja.s28687.validators.CarFreightValidator;
+import pl.edu.pja.s28687.validators.ICarLoadValidator;
 
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoadableRailroadCarTest {
-    private LocoBase locoBase;
-    private CarsFactory carsFactory;
-    private CarBuilder carBuilder;
-    private LoadFactory loadFactory;
-    private LoadBuilder loadBuilder;
 
-    @BeforeEach
-    void setUp() {
-        locoBase = new LocoBase();
-        carsFactory = new CarsFactory(locoBase);
-        carBuilder = new CarBuilder(locoBase);
-        loadFactory = new LoadFactory(locoBase);
-        loadBuilder = new LoadBuilder(locoBase);
+
+    private static final LocoBase LOCO_BASE = new LocoBase();
+    private static final ICarLoadValidator LOAD_VALIDATOR = new CarFreightValidator();
+    private static final LoadFactory LOAD_FACTORY = new LoadFactory(LOCO_BASE);
+    private static final CarsFactory CARS_FACTORY = new CarsFactory(LOCO_BASE);
+
+    @Test
+    void load_BasicFreight_ToBasicFreightCar_WhenBelowWeightLimit_ShouldReturnTrue() {
+        BasicFreightCar car = new BasicFreightCar(1, LOAD_VALIDATOR);
+        BasicFreightLoad load = new BasicFreightLoad(1);
+        assertTrue(car.getCurrentWeight().add(load.getWeight()).compareTo(car.getMaxWeight()) <= 0);
+        assertTrue(car.load(load));
+    }
+
+    @Test
+    void load_BasicFreight_ToBasicFreightCar_WhenAboveWeightLimit_ShouldThrowValidationException() {
+        BasicFreightCar car = new BasicFreightCar(1, LOAD_VALIDATOR);
+        BasicFreightLoad load = new BasicFreightLoad(20000000);
+        assertTrue(car.getCurrentWeight().add(load.getWeight()).compareTo(car.getMaxWeight()) > 0);
+        assertThrows(ValidationException.class, () -> car.load(load));
+    }
+
+    @Test
+    void load_HeavyFreight_ToBasicFreightCar_ShouldThrowValidationException() {
+        // used generic methods disallow us to directly load concrete incompatible load types to concrete car types
+        ILoadCarrier<IDeliverable> car = (ILoadCarrier<IDeliverable>) CARS_FACTORY.createCarOfType(CarType.BASIC_FREIGHT);
+        IDeliverable load = LOAD_FACTORY.createRandomLoadOfType(LoadType.HEAVY_FREIGHT);
+        assertTrue(car.getCurrentWeight().add(load.getWeight()).compareTo(car.getMaxWeight()) < 0);
+        assertThrows(ValidationException.class, () -> car.load(load));
+    }
+
+    @Test
+    void load_BasicFreight_ToHeavyFreightCar_ShouldThrowValidationException() {
+        // used generic methods disallow us to directly load concrete incompatible load types to concrete car types
+        ILoadCarrier<IDeliverable> car = (ILoadCarrier<IDeliverable>) CARS_FACTORY.createCarOfType(CarType.HEAVY_FREIGHT);
+        IDeliverable load = LOAD_FACTORY.createRandomLoadOfType(LoadType.BASIC_FREIGHT);
+        assertTrue(car.getCurrentWeight().add(load.getWeight()).compareTo(car.getMaxWeight()) < 0);
+        assertThrows(ValidationException.class, () -> car.load(load));
+    }
+
+    @Test
+    void load_LiquidLoad_ToHeavyFreightCar_ShouldThrowValidationException() {
+        // used generic methods disallow us to directly load concrete incompatible load types to concrete car types
+        ILoadCarrier<IDeliverable> car = (ILoadCarrier<IDeliverable>) CARS_FACTORY.createCarOfType(CarType.HEAVY_FREIGHT);
+        IDeliverable load = LOAD_FACTORY.createRandomLoadOfType(LoadType.LIQUID);
+        assertTrue(car.getCurrentWeight().add(load.getWeight()).compareTo(car.getMaxWeight()) < 0);
+        assertThrows(ValidationException.class, () -> car.load(load));
+    }
+
+    @Test
+    void load_LiquidToxicLoad_ToLiquidLoadCar_ShouldThrowValidationException() {
+        // used generic methods disallow us to directly load concrete incompatible load types to concrete car types
+        ILoadCarrier<IDeliverable> car = (ILoadCarrier<IDeliverable>) CARS_FACTORY.createCarOfType(CarType.LIQUID);
+        IDeliverable load = LOAD_FACTORY.createRandomLoadOfType(Set.of(LoadType.LIQUID, LoadType.TOXIC));
+        assertThrows(ValidationException.class, () -> car.load(load));
 
     }
 
     @Test
-    void load() {
-        LiquidLoadCar car = (LiquidLoadCar) carsFactory.createCarOfType(CarType.LIQUID);
-        LiquidLoad load = (LiquidLoad) loadFactory.createLoad(LoadType.LIQUID);
-        car.load(load);
-        assertEquals(load, car.getLoads().get(0));
-        IDeliverable load2 =  loadFactory.createLoad(LoadType.TOXIC);
-        LiquidLoad load3 = (LiquidLoad) loadBuilder.setWeight(999999).setFlags(Set.of(LoadType.LIQUID)).build();
-        car.load(load3);
-        assertFalse(car.getLoads().contains(load3));
-        LiquidLoad load4 = (LiquidLoad) loadBuilder.setWeight(1).setFlags(Set.of(LoadType.LIQUID)).build();
-        car.load(load4);
-        assertTrue(car.getLoads().contains(load4));
+    void load_LiquidToxicLoad_ToLiquidToxicLoadCar_ShouldReturnTrue() {
+        LiquidToxicLoadCar car = new LiquidToxicLoadCar(2, LOAD_VALIDATOR);
+        LiquidToxicLoad load = new LiquidToxicLoad(1, 2);
+        assertTrue(car.getCurrentWeight().add(load.getWeight()).compareTo(car.getMaxWeight()) < 0);
+        assertTrue(car.load(load));
     }
 
     @Test
-    void unLoad() {
-        LiquidLoadCar car = (LiquidLoadCar) carsFactory.createCarOfType(CarType.LIQUID);
-        LiquidLoad load = (LiquidLoad) loadFactory.createLoad(LoadType.LIQUID);
-        car.load(load);
+    void load_LiquidLoad_ToLiquidToxicLoadCar_ShouldThrowValidationException() {
+        // used generic methods disallow us to directly load concrete incompatible load types to concrete car types
+        ILoadCarrier<IDeliverable> car = (ILoadCarrier<IDeliverable>) CARS_FACTORY.createCarOfType(CarType.LIQUID_TOXIC);
+        IDeliverable load = LOAD_FACTORY.createRandomLoadOfType(Set.of(LoadType.LIQUID));
+        assertThrows(ValidationException.class, () -> car.load(load));
+    }
+
+    @Test
+    void unload_LoadedLoad_ShouldReturnTrue() {
+        BasicFreightCar car = new BasicFreightCar(1, LOAD_VALIDATOR);
+        BasicFreightLoad load = new BasicFreightLoad(1);
+        assertTrue(car.getCurrentWeight().add(load.getWeight()).compareTo(car.getMaxWeight()) <= 0);
+        assertTrue(car.load(load));
+        assertTrue(car.unLoad(load));
+    }
+
+    @Test
+    void unload_NotLoadedLoad_ShouldThrowValidationException() {
+        BasicFreightCar car = new BasicFreightCar(1, LOAD_VALIDATOR);
+        BasicFreightLoad load = new BasicFreightLoad(1);
+        assertThrows(ValidationException.class, () -> car.unLoad(load));
+    }
+
+    @Test
+    void unload_LoadedLoad_ShouldRemoveItFromLoadList() {
+        BasicFreightCar car = new BasicFreightCar(1, LOAD_VALIDATOR);
+        BasicFreightLoad load = new BasicFreightLoad(1);
+        assertTrue(car.getCurrentWeight().add(load.getWeight()).compareTo(car.getMaxWeight()) <= 0);
+        assertTrue(car.load(load));
         assertTrue(car.getLoads().contains(load));
-        car.unLoad(load);
+        assertTrue(car.unLoad(load));
         assertFalse(car.getLoads().contains(load));
-    }
-
-    @Test
-    void getCurrentWeight() {
-        LiquidLoadCar car = (LiquidLoadCar) carsFactory.createCarOfType(CarType.LIQUID);
-        LiquidLoad load = (LiquidLoad) loadFactory.createLoad(LoadType.LIQUID);
-        car.load(load);
-        assertEquals(load.getWeight().intValue(), car.getCurrentWeight().subtract(car.getNetWeight()).intValue());
-    }
-
-    @Test
-    void validateWeight() {
-        LiquidLoadCar car = (LiquidLoadCar) carsFactory.createCarOfType(CarType.LIQUID);
-        LiquidLoad load = (LiquidLoad) loadFactory.createLoad(LoadType.LIQUID);
-        assertTrue(car.validateWeight(load));
-        LiquidLoad load2 = (LiquidLoad) loadBuilder.setWeight(999999).setFlags(Set.of(LoadType.LIQUID)).build();
-        assertFalse(car.validateWeight(load2));
-    }
-
-    @Test
-    void validateFlags() {
-        LiquidLoadCar car = (LiquidLoadCar) carsFactory.createCarOfType(CarType.LIQUID);
-        IDeliverable load =  loadFactory.createLoad(LoadType.LIQUID);
-        assertTrue(car.validateFlags(load));
-        IDeliverable load2 =  loadFactory.createLoad(LoadType.TOXIC);
-        assertFalse(car.validateFlags(load2));
-        LoadableRailroadCar<?> car2 = (LoadableRailroadCar<?>) carsFactory.createCarOfType(CarType.LIQUID_TOXIC);
-        assertTrue(car2.validateFlags(load2));
-    }
-
-    @Test
-    void getAllowedLoadFlags() {
-        LiquidLoadCar car = (LiquidLoadCar) carsFactory.createCarOfType(CarType.LIQUID);
-        assertEquals(Set.of(LoadType.LIQUID), car.getAllowedLoadFlags());
-        LoadableRailroadCar<?> car2 = (LoadableRailroadCar<?>) carsFactory.createCarOfType(CarType.LIQUID_TOXIC);
-        assertEquals(Set.of(LoadType.LIQUID, LoadType.TOXIC), car2.getAllowedLoadFlags());
-    }
-
-    @Test
-    void setAllowedFlags() {
-        LiquidLoadCar car = (LiquidLoadCar) carsFactory.createCarOfType(CarType.LIQUID);
-        car.setAllowedFlags(Set.of(LoadType.TOXIC));
-        assertEquals(Set.of(LoadType.TOXIC), car.getAllowedLoadFlags());
-    }
-
-
-    @Test
-    void getLoads() {
-        LiquidLoadCar car = (LiquidLoadCar) carsFactory.createCarOfType(CarType.LIQUID);
-        LiquidLoad load = (LiquidLoad) loadFactory.createLoad(LoadType.LIQUID);
-        car.load(load);
-        assertEquals(1, car.getLoads().size());
-        assertEquals(load, car.getLoads().get(0));
-    }
-
-    @Test
-    void validateLoad() {
-        LiquidLoadCar car = (LiquidLoadCar) carsFactory.createCarOfType(CarType.LIQUID);
-        LiquidLoad load = (LiquidLoad) loadFactory.createLoad(LoadType.LIQUID);
-        assertTrue(car.validateLoad(load));
-        LiquidLoad load2 = (LiquidLoad) loadBuilder.setWeight(999999).setFlags(Set.of(LoadType.LIQUID)).build();
-        assertFalse(car.validateLoad(load2));
-        IDeliverable load3 =  loadFactory.createLoad(LoadType.TOXIC);
-        assertFalse(car.validateLoad(load3));
     }
 }
