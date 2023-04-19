3cb7a22ce6ffdb01a13539f7ad42e26303edc1cf
adam7 <adam7171512@gmail.com>
2023-04-19 14:25:00 +0200

    add gap filling option to railroad creation menu

--- src/main/java/pl/edu/pja/s28687/consoleInterface/objectCreationMenus/bulk/BulkRailroadCreation.java
+++ src/main/java/pl/edu/pja/s28687/consoleInterface/objectCreationMenus/bulk/BulkRailroadCreation.java
@@ -1,6 +1,7 @@
 package pl.edu.pja.s28687.consoleInterface.objectCreationMenus.bulk;
 
 import pl.edu.pja.s28687.consoleInterface.AbstractLeafMenu;
+import pl.edu.pja.s28687.factories.GapFiller;
 import pl.edu.pja.s28687.factories.RailroadsFactory;
 
 public class BulkRailroadCreation extends AbstractLeafMenu {
@@ -8,11 +9,12 @@ public class BulkRailroadCreation extends AbstractLeafMenu {
     public void menuSpecificAction() {
         System.out.println("1. Create random connections");
         System.out.println("2. Create connections based on proximity");
+        System.out.println("3. Fill Gaps");;
 
         int type;
         do {
             type = resourceContainer.parseToInt(scan.nextLine());
-        } while (type < 1 || type > 2);
+        } while (type < 1 || type > 3);
 
         RailroadsFactory railroadsFactory = resourceContainer.getRailroadsFactory();
 
@@ -42,6 +44,9 @@ public class BulkRailroadCreation extends AbstractLeafMenu {
                     System.err.println("Error while creating railroad links, likely not enough stations");
                     e.printStackTrace();
                 }
+            }
+            case 3 ->{
+                GapFiller.fill(resourceContainer.getLocoBase());
             }
         }
     }
