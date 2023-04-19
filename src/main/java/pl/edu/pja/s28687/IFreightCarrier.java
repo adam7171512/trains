3c267561efd775f503d74beff08f9bdcae154ee5
adam7 <adam7171512@gmail.com>
2023-04-19 14:03:13 +0200

    remove old GUI elements

--- src/main/java/pl/edu/pja/s28687/IFreightCarrier.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.pja.s28687;
-
-import pl.edu.pja.s28687.cars.ILoadCarrier;
-import pl.edu.pja.s28687.load.IBasicFreight;
-import pl.edu.pja.s28687.load.IDeliverable;
-
-public interface IFreightCarrier extends ILoadCarrier<IBasicFreight> {
-}
