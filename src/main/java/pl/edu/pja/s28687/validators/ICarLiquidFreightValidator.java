67b01c9365baa70445ee538a748f633cc7b1e888
adam7 <adam7171512@gmail.com>
2023-04-19 14:32:05 +0200

    remove unused imports

--- src/main/java/pl/edu/pja/s28687/validators/ICarLiquidFreightValidator.java
+++ src/main/java/pl/edu/pja/s28687/validators/ICarLiquidFreightValidator.java
@@ -1,11 +1,8 @@
 package pl.edu.pja.s28687.validators;
 
-import pl.edu.pja.s28687.cars.ILoadCarrier;
-import pl.edu.pja.s28687.load.IDeliverable;
+import pl.edu.pja.s28687.cars.ILiquidCarrier;
 import pl.edu.pja.s28687.load.ILiquid;
-import pl.edu.pja.s28687.load.Load;
 
 public interface ICarLiquidFreightValidator extends ICarLoadValidator{
-
     boolean validateVolume(ILiquid load, ILiquidCarrier receiver);
 }
