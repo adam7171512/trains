b9d2db162798110be21ed7afef8404b376751f66
adam7 <adam7171512@gmail.com>
2023-04-20 05:41:29 +0200

    change logging menu

--- src/main/java/pl/edu/pja/s28687/consoleInterface/LoggingMenu.java
+++ src/main/java/pl/edu/pja/s28687/consoleInterface/LoggingMenu.java
@@ -10,6 +10,7 @@ import pl.edu.pja.s28687.logistics.LocoBase;
 import java.util.logging.Level;
 
 public class LoggingMenu extends AbstractLeafMenu{
+    private boolean loggingEverything = false;
 
     private final AggregateLogger logger;
     public LoggingMenu(AggregateLogger logger) {
@@ -27,7 +28,8 @@ public class LoggingMenu extends AbstractLeafMenu{
             menu.append("1. Start logging train set reports to Appstate.txt");
         }
         menu.append("\n2. Print current train set report");
-        menu.append("\n3. Turn on logging everything on INFO level");
+        menu.append( loggingEverything ? "\n3. Stop logging everything to console"
+                : "\n3. Start logging everything to console");
         menu.append("\n0. Back");
         System.out.println(menu);
 
@@ -49,10 +51,20 @@ public class LoggingMenu extends AbstractLeafMenu{
                 System.out.println(TrainSetInfo.getAggregatedTrainSetsInfo(resourceContainer.getLocoBase()));
             }
             case 3 -> {
-                AbstractRailroadCar.setLogLevel(Level.INFO);
-                Locomotive.setLogLevel(Level.INFO);
-                Conductor.setLogLevel(Level.INFO);
-                LocoBase.setLogLevel(Level.INFO);
+                if (loggingEverything) {
+                    AbstractRailroadCar.setLogLevel(Level.SEVERE);
+                    Locomotive.setLogLevel(Level.SEVERE);
+                    Conductor.setLogLevel(Level.SEVERE);
+                    LocoBase.setLogLevel(Level.SEVERE);
+                    loggingEverything = false;
+                }
+                else {
+                    AbstractRailroadCar.setLogLevel(Level.INFO);
+                    Locomotive.setLogLevel(Level.INFO);
+                    Conductor.setLogLevel(Level.INFO);
+                    LocoBase.setLogLevel(Level.INFO);
+                    loggingEverything = true;
+                }
             }
             case 0 -> {}
         }
