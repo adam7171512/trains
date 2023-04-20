a1988ba0905261530d1f3c5c9b23ee9b13f29e30
adam7 <adam7171512@gmail.com>
2023-04-20 05:49:32 +0200

    add description to Main

--- src/main/java/pl/edu/pja/s28687/Main.java
+++ src/main/java/pl/edu/pja/s28687/Main.java
@@ -3,6 +3,15 @@ package pl.edu.pja.s28687;
 import pl.edu.pja.s28687.consoleInterface.ConsoleInterface;
 
 public class Main {
+    /**
+     * Please select "Presentation" option in the menu to see the presentation
+     * 1 pixel = 1 km.
+     * Time frequency and distance multiplier can be changed in the dispatch menu for more dynamic presentation.
+     * Skipping presentations.java file, as tests are provided.
+     * Logging is set to SEVERE by default, so it won't spam the console. You can change that option in the logging menu.
+     * Logging to Appstate.txt is set to OFF by default, so it won't spam the file. You can change that option in the logging menu.
+     */
+
     public static void main(String[] args) {
         ConsoleInterface consoleInterface = new ConsoleInterface();
         consoleInterface.show();
